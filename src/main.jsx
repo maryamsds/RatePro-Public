fc63c777e96c17f7a8522cc3f8f8c633c6160cd3
Arsalan <jaffri.arsalan786@gmail.com>
2025-04-26 13:34:52 +0500

    krte raho commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/React_Survanta_Frontend">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
